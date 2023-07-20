a209e863e80ee8951746e4186e8a29e94ce08af3
Dmitrii <111002830+qoiqvi@users.noreply.github.com>
2023-07-20 14:14:40 +0500

    Initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,24 @@
+import path from 'path'
+import { type Configuration } from 'webpack'
+import { buildWebpackConfig } from './config/webpack/buildWebpackConfig'
+import { type BuildEnv, type BuildPaths } from './config/webpack/types/config'
+
+export default (env: BuildEnv): Configuration => {
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html')
+  }
+
+  const mode = env.mode || 'development'
+  const port = env.port || 3000
+  const isDev = mode === 'development'
+
+  const config: Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port
+  })
+  return config
+}
